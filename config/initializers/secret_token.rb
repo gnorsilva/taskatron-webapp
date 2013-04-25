cf0c0d4244eb6c07da589d95e717cfe11c689770
Gonçalo Silva <gnorsilva@gmail.com>
2013-04-25 21:45:36 +0100

    initial rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Taskatron::Application.config.secret_token = '6da3a45a388d117f2880d779d5a3c351ef1fbc5854fde19b4fe0d932a82faef2c53f77649e0a573ea9b73277f596041c58ee4d1402dc2f60f8ba46ddc002fad8'
